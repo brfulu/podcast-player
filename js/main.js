606d63ba5be88d229c2f1f1a773c7baa6c873990
branko_fu <brankofulurija@gmail.com>
2019-05-25 10:20:09 +0200

    Add settings page

--- js/main.js
+++ js/main.js
@@ -50,5 +50,42 @@ $(document).ready(function () {
     }
   });
 
+  /* chart.js chart examples */
+
+  // chart colors
+  var colors = ['#007bff', '#28a745', '#333333', '#c3e6cb', '#dc3545', '#6c757d'];
+
+  /* large line chart */
+  var chLine = document.getElementById("chLine");
+  var chartData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+      data: [35, 25, 56, 5, 17, 89, 43],
+      backgroundColor: 'gainsboro',
+      borderColor: colors[0],
+      borderWidth: 4,
+      pointBackgroundColor: colors[0]
+    }]
+  };
+
+  if (chLine) {
+    new Chart(chLine, {
+      type: 'line',
+      data: chartData,
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: false
+            }
+          }]
+        },
+        legend: {
+          display: false
+        }
+      }
+    });
+  }
+
 });
 
