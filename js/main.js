287b69137caabb62ed161b5adcba58f23c09d089
branko_fu <brankofulurija@gmail.com>
2019-06-08 14:45:30 +0200

    Fix carousel

--- js/main.js
+++ js/main.js
@@ -43,12 +43,12 @@ $(document).ready(function () {
     $('#recovery-alert').show();
   })
 
-  $('#carouselExample').on('slide.bs.carousel', function (e) {
 
+  $('.carousel').on('slide.bs.carousel', function (e) {
     var $e = $(e.relatedTarget);
     var idx = $e.index();
     var itemsPerSlide = 4;
-    var totalItems = $('.carousel-item').length;
+    var totalItems = 8;
 
     if (idx >= totalItems - (itemsPerSlide - 1)) {
       var it = itemsPerSlide - (totalItems - idx);
@@ -63,6 +63,7 @@ $(document).ready(function () {
       }
     }
   });
+
 
   /* chart.js chart examples */
 
