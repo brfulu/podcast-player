758dedb8817144bc7c0002cc137542f8968c8a9e
branko_fu <brankofulurija@gmail.com>
2019-06-13 09:03:04 +0200

    Fix carousel bug

--- js/main.js
+++ js/main.js
@@ -37,28 +37,21 @@ $(document).ready(function () {
     $('a[aria-expanded=true]').attr('aria-expanded', 'false');
   });
 
-  $('#recovery-alert').hide();
-
-  $('#recovery-btn').click(function () {
-    $('#recovery-alert').show();
-  })
-
 
   $('.carousel').on('slide.bs.carousel', function (e) {
     var $e = $(e.relatedTarget);
     var idx = $e.index();
     var itemsPerSlide = 4;
     var totalItems = 10;
-
     if (idx >= totalItems - (itemsPerSlide - 1)) {
       var it = itemsPerSlide - (totalItems - idx);
       for (var i = 0; i < it; i++) {
         // append slides to end
         if (e.direction == "left") {
-          $('.carousel-item').eq(i).appendTo('.carousel-inner');
+          $(this).find('*').filter('.carousel-item').eq(i).appendTo('.carousel-inner');
         }
         else {
-          $('.carousel-item').eq(0).appendTo('.carousel-inner');
+          $(this).find('*').filter('.carousel-item').eq(0).appendTo($(this).children().filter('.carousel-inner'));
         }
       }
     }
